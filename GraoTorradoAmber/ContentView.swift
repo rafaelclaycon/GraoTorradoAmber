e6d66a73b3b63771b62460d8041a91c7ebf4226c
Rafael Claycon Schmitt <rafael.claycon@gmail.com>
2020-09-26 08:59:14 -0300

    Primeiro commit

---
+++ GraoTorradoAmber/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GraoTorradoAmber
+//
+//  Created by Rafael Schmitt on 26/09/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GraoTorradoAmber/GraoTorradoAmberApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraoTorradoAmberApp.swift
+//  GraoTorradoAmber
+//
+//  Created by Rafael Schmitt on 26/09/20.
+//
+
+import SwiftUI
+
+@main
+struct GraoTorradoAmberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
